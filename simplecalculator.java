f5f5ce9cb2ad3434fc86e149a07fb7dcb516137d
lathishkumar2 <klathish08@gmail.com>
2024-09-30 18:11:59 +0530

    custom error handling-exception

--- simplecalculator.java
+++ simplecalculator.java
@@ -5,6 +5,9 @@ import java.util.*;
 public class simplecalculator {
 
     public static void main(String[] args) {
+       
+        
+       
         System.out.print("enter the numbers :");
      Scanner input1 = new Scanner (System.in);
         int num1 = input1.nextInt();
@@ -14,8 +17,11 @@ public class simplecalculator {
         System.out.println("SUB");
         System.out.println("MUL");
         System.out.println("DIV");
+        System.out.println("Exit");
         System.out.print("which one : ");
-    Scanner input2 = new Scanner (System.in);
+       
+        do{
+            Scanner input2 = new Scanner (System.in);
         String str=input2.nextLine();
         char chr = str.charAt(0);
         if(chr=='a'||chr== 'A'){
@@ -25,10 +31,29 @@ public class simplecalculator {
         }else if (chr=='m'||chr== 'M') {
             System.out.println(" multipication :"+(num1*num2));
         }else if (chr=='d'||chr== 'D')  {
-            System.out.println(" division :"+(num1/num2));
-        }else {
+            try {
+                System.out.println(" division :"+(num1/num2));
+            } catch (ArithmeticException e) {
+                System.out.println("Error: Division by zero");
+            }
+           
+        }else if (chr=='e'||chr=='E') {
+            System.out.println("thank for coming");
+        }     
+        
+        else {
             System.out.println("option is not there😅😊");
         }
+        System.out.println("did u want calculate again :");
+        System.out.println("YES");
+        System.out.println("NO");
+        Scanner input3 = new Scanner (System.in);
+        String str1=input3.nextLine();
+         char chr1 = str1.charAt(0);
+    }
+        while(true);
+
+
     
 
         
